e1cef34df571d4c255f895ee41a8b0be681e9d20
SamuelValentin <samuellv89@gmail.com>
2023-02-03 14:21:23 -0300

    init das paginas

---
+++ Views/about.view.php
@@ -0,0 +1,10 @@
+<?php require('partials/head.php');?>
+
+    <div class="bg-gray-50 text-gray-800 py-20 px-6">
+        <h1 class="text-5xl font-bold mt-0 mb-6"><?= $name ?></h1>
+        <h3 class="text-3xl font-bold mb-8">Flamengo</h3>
+        <p>Estamos na nossa historia</p>
+    </div>
+
+</body>
+</html>
---
+++ Views/partials/head.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>TESTE</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+    
+<?php require('nav.php');?>
